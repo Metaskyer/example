b531610031a86813bbae410d10ff39e19d1c5f8c
kvizero <metaskyer@gmail.com>
2021-04-23 01:22:02 +0900

    properties files be deleted

--- src/main/java/kr/ktservice/example/WebSecurityConfig.java
+++ src/main/java/kr/ktservice/example/WebSecurityConfig.java
@@ -17,7 +17,7 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
     protected void configure(HttpSecurity http) throws Exception {
         http
                 .authorizeRequests()
-                .antMatchers("/", "/home").permitAll()
+                .antMatchers("/", "/index").permitAll()
                 .anyRequest().authenticated()
                 .and()
                 .formLogin()
@@ -25,7 +25,7 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
                 .permitAll()
                 .and()
                 .logout()
-                .logoutSuccessUrl("/home")
+                .logoutSuccessUrl("/index")
                 .permitAll();
     }
 
