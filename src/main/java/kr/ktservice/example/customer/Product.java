6b39f065713a67e8304645ddf05f2a7dc6d1e7e4
kvizero <metaskyer@gmail.com>
2021-05-08 17:11:02 +0900

    FIELDS UPDATE

--- src/main/java/kr/ktservice/example/customer/Product.java
+++ src/main/java/kr/ktservice/example/customer/Product.java
@@ -1,12 +1,16 @@
 package kr.ktservice.example.customer;
 
 import kr.ktservice.example.model.NamedEntity;
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import javax.persistence.*;
 
 @Entity
 @Data
+@AllArgsConstructor
+@NoArgsConstructor
 @Table(name = "products")
 public class Product extends NamedEntity {
 
@@ -20,5 +24,13 @@ public class Product extends NamedEntity {
     @ManyToOne
     @JoinColumn(name = "type_id")
     private ProductType type_id;
+
+    private int qty;
+    private double price;
+    private double amount;
+    private String address1;
+    private String address2;
+
+
 
 }
